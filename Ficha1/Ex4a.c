1739f92c22e7bdef2fb8fb5e7edc2a08c4515d24
RubenBMS <a32338@alunos.ipca.pt>
2024-10-15 20:46:41 +0100

    Ex4a

---
+++ Ficha1/Ex4a.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+void main()
+{
+    float taxa = 1.17;
+    float valorEuros;
+    float valorDollars;
+
+
+    printf("Introduza valor em euros: ");
+    scanf("%f",&valorEuros);
+
+    valorDollars = valorEuros * taxa ;
+
+    printf("Valor em dollars= %f.", valorDollars);
+
+
+
+}
