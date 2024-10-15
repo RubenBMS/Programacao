342e6fbb93e5a37781e9e2d83e716ae416b5c27a
RubenBMS <a32338@alunos.ipca.pt>
2024-10-15 19:43:51 +0100

    Update Ex1.c

--- Ficha1/Ex1.c
+++ Ficha1/Ex1.c
@@ -7,9 +7,15 @@ void main()
     float T2;
     
     char aluno[100];
+    char disciplina[100];
+
+    printf("Pretende efetuar o calculo para que disciplina: ");
+    scanf("%s", disciplina);
 
     printf("Insira o nome do aluno: ");
     scanf("%s", aluno);
+
+
 
     printf("Insira a nota do teste 1: ");
     scanf("%f", &T1);
@@ -19,5 +25,5 @@ void main()
 
     NF = T1 * 0.4 + T2 * 0.6 ;
 
-    printf("A nota final do aluno %s e %f.",aluno, NF);
+    printf("A nota final de %s do aluno %s e %f.",disciplina, aluno, NF);
 }
